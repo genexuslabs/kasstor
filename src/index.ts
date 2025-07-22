c5e5ece9b8d645c0838e3bd0efa69b596b24505b
ncamera@genexus.com <ncamera@genexus.com>
2025-07-22 14:05:20 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -8,9 +8,11 @@ export type {
 } from "./decorators/Component/types";
 export { Event, EventEmitter } from "./decorators/Event/index";
 export { Watch } from "./decorators/Watch/index";
+export type { WatchOptions } from "./decorators/Watch/types";
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 //                           Other
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 export { insertIntoIndex, removeIndex } from "./other/array";
 export { SyncWithRAF } from "./other/sync-with-frames";
+
