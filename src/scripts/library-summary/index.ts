e5a070d05f799fd98e85e30e76196fb6f435aad3
ncamera <ncamera@genexus.com>
2025-09-03 23:35:05 -0300

    Rename functions

--- src/scripts/library-summary/index.ts
+++ src/scripts/library-summary/index.ts
@@ -2,14 +2,14 @@ import { writeFile } from "fs/promises";
 import { join } from "path";
 import { extractLibraryComponents } from "./generator";
 
-export const getLibrarySchema = (relativeComponentsSrcPath: string) =>
+export const getLibrarySummary = (relativeComponentsSrcPath: string) =>
   extractLibraryComponents(join(process.cwd(), relativeComponentsSrcPath));
 
-export const generateLibrarySchema = async (
+export const generateLibrarySummary = async (
   relativeComponentsSrcPath: string,
   resultFilePath = "library-schema.ts"
 ) => {
-  const result = await getLibrarySchema(relativeComponentsSrcPath);
+  const result = await getLibrarySummary(relativeComponentsSrcPath);
 
   return writeFile(
     resultFilePath,
