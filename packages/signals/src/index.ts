51bcd3dd10bd1761d2542aa058bde61d2f302adc
Nicolás Cámera <ncamera@genexus.com>
2025-11-30 19:22:28 -0300

    Create index.ts

---
+++ packages/signals/src/index.ts
@@ -0,0 +1,39 @@
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//                           Core
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+export { batch } from "./core/batch/index.js";
+export {
+  computed,
+  effect,
+  effectScope,
+  endBatch,
+  getActiveSub,
+  getBatchDepth,
+  isComputed,
+  isEffect,
+  isEffectScope,
+  isSignal,
+  setActiveSub,
+  signal,
+  startBatch,
+  trigger
+} from "./core/index.js";
+export { untracked } from "./core/untracked/index.js";
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//                        Directives
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+export {
+  watch,
+  WatchDirective,
+  type WatchDirectiveFunction
+} from "./directives/watch/index.js";
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//                           Types
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+export type {
+  KasstorSignal,
+  KasstorSignalComputed,
+  KasstorSignalState
+} from "./typings/types";
