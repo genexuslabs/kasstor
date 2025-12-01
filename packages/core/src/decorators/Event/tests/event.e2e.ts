488a92ef4a6122b54803b5a62471878032b5d78e
ncamera <ncamera@genexus.com>
2025-12-01 18:19:56 -0300

    Refactor the test hierarchy

--- packages/core/src/decorators/Event/tests/event.e2e.ts
+++ packages/core/src/decorators/Event/tests/event.e2e.ts
@@ -33,67 +33,69 @@ class DummyComponent extends LitElement {
 
 customElements.define("geai-event-dummy-component", DummyComponent);
 
-describe(`[Decorator | Event]`, () => {
-  let dummyComponentRef: DummyComponent;
+describe(`[Decorator]`, () => {
+  describe(`[Event]`, () => {
+    let dummyComponentRef: DummyComponent;
 
-  let event1Handler = (event: CustomEvent) => {
-    event1DetailChecker(event.detail);
-  };
-  let event1CancellableHandler = (event: CustomEvent) => {
-    event1DetailChecker(event.detail);
-  };
-  let event1DetailChecker = vi.fn();
-  let callbackIfNotCancelled = vi.fn();
-
-  beforeEach(async () => {
-    // Redefine the mocks before running each test
-    event1Handler = (event: CustomEvent) => {
+    let event1Handler = (event: CustomEvent) => {
       event1DetailChecker(event.detail);
     };
-    event1CancellableHandler = (event: CustomEvent) => {
-      event.preventDefault();
+    let event1CancellableHandler = (event: CustomEvent) => {
       event1DetailChecker(event.detail);
     };
-    event1DetailChecker = vi.fn();
-    callbackIfNotCancelled = vi.fn();
-  });
-  afterEach(cleanup);
+    let event1DetailChecker = vi.fn();
+    let callbackIfNotCancelled = vi.fn();
 
-  const renderTemplate = async (prevent = false) => {
-    render(
-      html`<geai-event-dummy-component
-        @event1=${prevent ? event1CancellableHandler : event1Handler}
-      ></geai-event-dummy-component>`
-    );
+    beforeEach(async () => {
+      // Redefine the mocks before running each test
+      event1Handler = (event: CustomEvent) => {
+        event1DetailChecker(event.detail);
+      };
+      event1CancellableHandler = (event: CustomEvent) => {
+        event.preventDefault();
+        event1DetailChecker(event.detail);
+      };
+      event1DetailChecker = vi.fn();
+      callbackIfNotCancelled = vi.fn();
+    });
+    afterEach(cleanup);
 
-    dummyComponentRef = document.querySelector("geai-event-dummy-component")!;
-    await dummyComponentRef.updateComplete;
-  };
+    const renderTemplate = async (prevent = false) => {
+      render(
+        html`<geai-event-dummy-component
+          @event1=${prevent ? event1CancellableHandler : event1Handler}
+        ></geai-event-dummy-component>`
+      );
 
-  it(`should emit the event1 one time`, async () => {
-    renderTemplate();
-    dummyComponentRef.fireEvent1WithDetail("test");
-    expect(event1DetailChecker).toBeCalledTimes(1);
-  });
+      dummyComponentRef = document.querySelector("geai-event-dummy-component")!;
+      await dummyComponentRef.updateComplete;
+    };
 
-  it(`should emit the event1 with detail = "test"`, async () => {
-    renderTemplate();
-    dummyComponentRef.fireEvent1WithDetail("test");
-    expect(event1DetailChecker).toBeCalledWith("test");
-  });
+    it(`should emit the event1 one time`, async () => {
+      renderTemplate();
+      dummyComponentRef.fireEvent1WithDetail("test");
+      expect(event1DetailChecker).toBeCalledTimes(1);
+    });
 
-  it(`should not prevent the event 1 by default`, async () => {
-    renderTemplate();
-    dummyComponentRef.fireEvent1WithPrevent(callbackIfNotCancelled);
-    expect(event1DetailChecker).toBeCalledTimes(1);
-    expect(callbackIfNotCancelled).toBeCalledTimes(1);
-  });
+    it(`should emit the event1 with detail = "test"`, async () => {
+      renderTemplate();
+      dummyComponentRef.fireEvent1WithDetail("test");
+      expect(event1DetailChecker).toBeCalledWith("test");
+    });
 
-  it(`should prevent the event 1 when calling preventDefault in the handler`, async () => {
-    renderTemplate(true);
-    dummyComponentRef.fireEvent1WithPrevent(callbackIfNotCancelled);
-    expect(event1DetailChecker).toBeCalledTimes(1);
-    expect(callbackIfNotCancelled).toBeCalledTimes(0);
+    it(`should not prevent the event 1 by default`, async () => {
+      renderTemplate();
+      dummyComponentRef.fireEvent1WithPrevent(callbackIfNotCancelled);
+      expect(event1DetailChecker).toBeCalledTimes(1);
+      expect(callbackIfNotCancelled).toBeCalledTimes(1);
+    });
+
+    it(`should prevent the event 1 when calling preventDefault in the handler`, async () => {
+      renderTemplate(true);
+      dummyComponentRef.fireEvent1WithPrevent(callbackIfNotCancelled);
+      expect(event1DetailChecker).toBeCalledTimes(1);
+      expect(callbackIfNotCancelled).toBeCalledTimes(0);
+    });
   });
 });
 
