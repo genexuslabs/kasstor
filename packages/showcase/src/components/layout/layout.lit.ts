a1d4c01feeca0005ff9619a86f14019d79cf5526
Nicolas Camera Lopez <ncamera@genexus.com>
2025-12-24 22:43:15 -0300

    Update layout.lit.ts

--- packages/showcase/src/components/layout/layout.lit.ts
+++ packages/showcase/src/components/layout/layout.lit.ts
@@ -6,19 +6,27 @@ import {
 import { html } from "lit";
 import styles from "./layout.scss?inline";
 
+import { state } from "lit/decorators.js";
+import "../playground/playground.lit";
+
 @Component({
   styles,
   tag: "kst-layout"
 })
 export class KstLayout extends SSRLitElement {
-  hotReplacedCallback() {
-    // this should kick off re-rendering
-    this.requestUpdate();
-  }
+  @state() value: string = "1";
 
   override render() {
-    return html`<div>123</div>
-      <input type="text" />`;
+    return html`<div>aa</div>
+      <kst-playground></kst-playground>
+      <select
+        @change=${(e: Event) =>
+          (this.value = (e.target as HTMLSelectElement).value)}
+      >
+        <option value="1" .selected=${this.value === "1"}>option 1</option>
+        <option value="2" .selected=${this.value === "2"}>option 2</option>
+        <option value="3" .selected=${this.value === "3"}>option 3</option>
+      </select>`;
   }
 }
 
