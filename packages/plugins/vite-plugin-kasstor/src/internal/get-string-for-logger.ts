0419e175b882cea37f56c37e3d43db992745f041
Nicolas Camera Lopez <ncamera@genexus.com>
2025-12-31 12:11:36 -0300

    Introduce line break to improve the logger's style

--- packages/plugins/vite-plugin-kasstor/src/internal/get-string-for-logger.ts
+++ packages/plugins/vite-plugin-kasstor/src/internal/get-string-for-logger.ts
@@ -1,13 +1,25 @@
 import { styleText } from "util";
 import { prettyTimeMark } from "./pretty-time-mark.js";
 
+let lastUpdateTime = performance.now();
+
+const ADD_END_LINE_DEBOUNCE = 1500;
+
 export const getStringForLogger = (
   operationType: string,
   components: string[],
   elapsedTime: number
-) =>
-  styleText("dim", "[kasstor] ") +
-  `updated ${operationType}: ` +
-  styleText("cyan", components.join(", ")) +
-  styleText("dim", ` in ${prettyTimeMark(elapsedTime)}`);
+) => {
+  const now = performance.now();
+  const timeSinceLastUpdate = now - lastUpdateTime;
+  lastUpdateTime = now;
+
+  return (
+    (timeSinceLastUpdate > ADD_END_LINE_DEBOUNCE ? "\n" : "") +
+    styleText("dim", "[kasstor] ") +
+    `updated ${operationType}: ` +
+    styleText("cyan", components.join(", ")) +
+    styleText("dim", ` in ${prettyTimeMark(elapsedTime)}`)
+  );
+};
 
