8aadf993c805e56ea80eea585a17f8c1176c9141
Nicolás Cámera <ncamera@genexus.com>
2026-01-05 16:43:10 -0300

    Delete old config files

--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-import { defineDistributionConfiguration } from "./vite-config-distribution";
-
-export default defineConfig(({ mode }) =>
-  defineDistributionConfiguration(
-    mode.startsWith("node"),
-    mode.endsWith("production")
-  )
-);
