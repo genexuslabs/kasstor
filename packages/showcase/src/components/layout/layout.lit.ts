4effb016b996a241a7714576958ab71222513c6b
Nicolás Cámera <ncamera@genexus.com>
2026-01-05 16:53:08 -0300

    Fix showcase start

--- packages/showcase/src/components/layout/layout.lit.ts
+++ packages/showcase/src/components/layout/layout.lit.ts
@@ -1,23 +1,22 @@
 import {
   Component,
-  SSRLitElement
+  KasstorElement
 } from "@genexus/kasstor-core/decorators/component.js";
 
 import { html } from "lit";
 import styles from "./layout.scss?inline";
 
 import { state } from "lit/decorators.js";
-import "../playground/playground.lit";
 
 @Component({
   styles,
   tag: "kst-layout"
 })
-export class KstLayout extends SSRLitElement {
+export class KstLayout extends KasstorElement {
   @state() value: string = "1";
 
   override render() {
-    return html`<div>aa</div>
+    return html`<div>aasdasda</div>
       <kst-playground></kst-playground>
       <select
         @change=${(e: Event) =>
@@ -27,6 +26,36 @@ export class KstLayout extends SSRLitElement {
         <option value="2" .selected=${this.value === "2"}>option 2</option>
         <option value="3" .selected=${this.value === "3"}>option 3</option>
       </select>`;
+  }
+}
+
+// ######### Auto generated bellow #########
+
+declare global {
+  // prettier-ignore
+  interface HTMLKstLayoutElementCustomEvent<T> extends CustomEvent<T> {
+    detail: T;
+    target: HTMLKstLayoutElement;
+  }
+
+  // prettier-ignore
+  interface HTMLKstLayoutElement extends KstLayout {
+    // Extend the KstLayout class redefining the event listener methods to improve type safety when using them
+    addEventListener<K extends keyof DocumentEventMap>(type: K, listener: (this: Document, ev: DocumentEventMap[K]) => unknown, options?: boolean | AddEventListenerOptions): void;
+    addEventListener<K extends keyof HTMLElementEventMap>(type: K, listener: (this: HTMLElement, ev: HTMLElementEventMap[K]) => unknown, options?: boolean | AddEventListenerOptions): void;
+    addEventListener(type: string, listener: EventListenerOrEventListenerObject, options?: boolean | AddEventListenerOptions): void;
+    
+    removeEventListener<K extends keyof DocumentEventMap>(type: K, listener: (this: Document, ev: DocumentEventMap[K]) => unknown, options?: boolean | EventListenerOptions): void;
+    removeEventListener<K extends keyof HTMLElementEventMap>(type: K, listener: (this: HTMLElement, ev: HTMLElementEventMap[K]) => unknown, options?: boolean | EventListenerOptions): void;
+    removeEventListener(type: string, listener: EventListenerOrEventListenerObject, options?: boolean | EventListenerOptions): void;
+  }
+
+  interface IntrinsicElements {
+    "kst-layout": HTMLKstLayoutElement;
+  }
+
+  interface HTMLElementTagNameMap {
+    "kst-layout": HTMLKstLayoutElement;
   }
 }
 
