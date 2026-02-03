3e04214c313c301ff3db019ca9bba698a6a34da3
Nicolas Camera Lopez <ncamera@genexus.com>
2026-02-03 12:43:27 -0300

    `[build]` Fix typo "bellow" to "below" in auto-generated content

--- packages/build/src/global-type-declarations/constants.ts
+++ packages/build/src/global-type-declarations/constants.ts
@@ -2,4 +2,5 @@
  * Marker comment that indicates where auto-generated content begins
  */
 export const AUTO_GENERATED_MARKER =
-  "// ######### Auto generated bellow #########";
+  "// ######### Auto generated below #########";
+
